d3968e3e2ee6623f216c1bcf66d84c4431f34cc5
mattias josephsson <josephson199@gmail.com>
2017-10-29 17:30:15 +0100

    Added web ui proj

---
+++ InfrastructureTests/BlogStoreTests.cs
@@ -0,0 +1,19 @@
+﻿using DataStore;
+using DataStore.Models;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using Xunit;
+
+namespace InfrastructureTests
+{
+    public class BlogStoreTests
+    {
+        [Fact]
+        public void SavePostTest()
+        {
+           
+            
+        }
+    }
+}
