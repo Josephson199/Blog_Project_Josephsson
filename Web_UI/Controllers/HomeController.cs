1d839afd5a5dd51b8bb603131e70fdcc7f23faa8
mattias josephsson <josephson199@gmail.com>
2017-10-29 18:11:51 +0100

    Added layout and index page

---
+++ Web_UI/Controllers/HomeController.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using DataStore;
+using Web_UI.Models.Home;
+using DataStore.Models;
+
+namespace Web_UI.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly BlogDataStore _dataStore;
+        const string StorageFolder = "BlogFiles";
+
+        public IEnumerable<PostSummaryModel> PostSummaries { get; private set; }
+
+        public HomeController(BlogDataStore dataStore)
+        {
+            _dataStore = dataStore;
+        }
+
+        public IActionResult Index()
+        {
+            IEnumerable<BlogPost> postModels = _dataStore.GetAllPosts().Where(p => p.IsPublic && !p.IsDeleted);
+
+            PostSummaries = postModels.Select(p => new PostSummaryModel
+            {
+                Id = p.Id,
+                Title = p.Title,
+                PublishTime = p.PubDate,
+                CommentCount = p.Comments.Where(c => c.IsPublic).Count(),
+            });
+
+            var viewModel = new IndexViewModel
+            {
+                PostSummaries = PostSummaries
+            };
+
+            return View(viewModel);
+        }
+    }
+}
--- Web_UI/Startup.cs
+++ Web_UI/Startup.cs
@@ -1,11 +1,7 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-using Microsoft.AspNetCore.Builder;
+﻿using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.DependencyInjection;
+using DataStore;
 
 namespace Web_UI
 {
@@ -15,6 +11,10 @@ namespace Web_UI
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddMvc();
+
+            services.AddSingleton<IFileSystem, PhysicalFileSystem>();
+            services.AddSingleton<BlogDataStore>();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -23,11 +23,20 @@ namespace Web_UI
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
+                app.UseBrowserLink();
+                app.UseDatabaseErrorPage();
+            }
+            else
+            {
+                app.UseExceptionHandler("/Error");
             }
 
-            app.Run(async (context) =>
+            app.UseStaticFiles();
+            app.UseMvc(routes =>
             {
-                await context.Response.WriteAsync("Hello World!");
+                routes.MapRoute(
+                    name: "default",
+                    template: "{controller=Home}/{action=Index}/{id?}");
             });
         }
     }
